59bf8a684d9638618e08e9ec9a4c4197bd070d3f
Fabiano Forte Ferreira <fabiano.fforte@gmail.com>
2024-02-13 23:33:39 -0300

    feat: add initial service booking

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -43,7 +43,7 @@ const barbershop = await db.barbershop.findUnique({
 
             <div className="px-5 flex flex-col gap-4 py-6">
                 {barbershop.services.map(service => (
-                    <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} />
+                    <ServiceItem key={service.id} barbershop={barbershop} service={service} isAuthenticated={!!session?.user} />
                 ))}
             </div>
         </div>
